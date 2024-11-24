cd903c55b4f16d3cc885cd86b8e7201dd107076a
Arvand <arv.marandi@gmail.com>
2024-11-24 14:05:49 -0800

    json listener

--- src/listener/file_listener.m
+++ src/listener/file_listener.m
@@ -1,13 +1,13 @@
-filename = 'C:\Users\arvma\sd\irrigators\src\listener\example.json'; % configure path to system
+jsonFile = 'C:\Users\arvma\sd\irrigators\src\listener\example.json'; % configure path to system
 
 % checks if the file exists
-if ~isfile(filename)
-    error('File "%s" does not exist. Please provide a valid file.', filename);
+if ~isfile(jsonFile)
+    error('File "%s" does not exist. Please provide a valid file.', jsonFile);
 end
 
 % reads the latest watering status value in the JSON file
 try
-    jsonData = jsondecode(fileread(filename));
+    jsonData = jsondecode(fileread(jsonFile));
     if ~isfield(jsonData, 'watering')
         error('Field "watering" not found in the JSON file.');
     end
@@ -16,3 +16,19 @@ try
 catch ME
     error('Error reading JSON file: %s', ME.message);
 end
+
+% file listener loop
+
+while true 
+    pause(5) % check every 5 seconds
+    
+    jsondecode(fileread(jsonFile)); % refresh the data
+    wateringValue = jsonData.watering;
+    if wateringValue ~= lastWateringValue 
+        lastWateringValue = wateringValue; % update the watering status when needed
+        disp("change in watering status")
+    end
+
+end
+
+
