cfe8e7b67732431cc7e7940cf141702fc1a26169
aster-isk <spinkmaddie00@gmail.com>
2024-12-03 00:19:22 -0800

    Fixed PHP and Page to run on Linux

--- webserver/html/updatewaterswitch.php
+++ webserver/html/updatewaterswitch.php
@@ -3,10 +3,16 @@ $json_string = json_encode($_POST);
 $sucess = 1;
 
 if(($_POST["status"] == 0 || $_POST["status"] == 1) && count($_POST) == 1) {
-  $file_handle = fopen('test.json', 'w');
-  fwrite($file_handle, $json_string);
-  fclose($file_handle);
-  $sucess = 0;
+  try {
+    $file_handle = fopen('/var/irrilog/wateringstat.json', 'w');
+    fwrite($file_handle, $json_string);
+    fclose($file_handle);
+    $sucess = 0;
+  }
+  
+  catch(Exception $e) {
+    $sucess = $e;
+  }
 }
 
 echo $sucess;
