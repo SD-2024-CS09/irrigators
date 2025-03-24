0b41dd7265519f566c4feca0bd6b76a3209b1e83
CalebLefcort <clefcort@zagmail.gonzaga.edu>
2025-03-24 16:47:24 -0700

    added < 0 check

--- test/SensorSym.py
+++ test/SensorSym.py
@@ -27,19 +27,23 @@ def read_ts(url):
          return None
 
 if __name__ == "__main__":
+    TS_WRITE_LIMIT = 16
     read_sensor_values = "https://api.thingspeak.com/channels/2667716/fields/1.json?results=1"
     write_sensor_values = "https://api.thingspeak.com/update?api_key=6AO4PYC5RM7D1KJX&field1="
 
     read_SM_decsion = "https://api.thingspeak.com/channels/2756308/fields/1.json?results=1"
     write_SM_decsion = "https://api.thingspeak.com/update?api_key=LEOTSB0DZBX02WH1&field1="
 
-    last_write = datetime.datetime.now() - datetime.timedelta(seconds=16)
+    last_write = datetime.datetime.now() - datetime.timedelta(seconds=TS_WRITE_LIMIT)
     while True:
         time.sleep(.5)
         choice = input('\n1. Write sensor value\n2. Read SM Decision\n3. Quit\nEnter choice: ')
         if choice == '1':
             val = input('Enter floating point value: ')
-            if((datetime.datetime.now() - last_write).total_seconds() > 16):
+            if (float(val) < 0.0):
+                print("Value must be a positive float\n")
+                continue
+            if((datetime.datetime.now() - last_write).total_seconds() > TS_WRITE_LIMIT):
                 try:
                     resp = write_ts(write_sensor_values, float(val))
                     last_write = datetime.datetime.now()
@@ -47,9 +51,10 @@ if __name__ == "__main__":
                     if resp == 200:
                         print("Value written to ThingSpeak please wait 16 seconds before writting again\n")
                 except ValueError:
-                    print("Value must be a float\n")
+                    print("Value must be a positive float\n")
             else:
-                print(f"must wait {16 - (datetime.datetime.now() - last_write).total_seconds()} more seconds before writting again\n")
+                remaining_secs = TS_WRITE_LIMIT - (datetime.datetime.now() - last_write).total_seconds()
+                print(f"must wait {remaining_secs} more seconds before writting again\n")
         elif choice == '2':
             data = read_ts(read_SM_decsion)
             print(f"StateMachine decision is {data['feeds'][0]['field1']}\n")
