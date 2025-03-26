598f6dd31af126f8e7abab6f93c9bd10efd5be95
aster-isk <spinkmaddie00@gmail.com>
2025-03-25 21:54:30 -0700

    Added form for bounds update.

--- webserver/html/index.php
+++ webserver/html/index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Irrigator Irrigation</title>
+  <title>Irrigator Irrigation System</title>
   <style>
     .banner {
       background: #AAAAAA;
@@ -35,40 +35,97 @@
         transform: rotate(360deg);
       }
     }
+    
+    #errorOutput {
+      width: 100%;
+      height: 50px;
+    }
   </style>
 </head>
 
 <body>
   <div class="banner">
-    <h1>Irrigator Irrigation</h1>
+    <h1>Irrigator Irrigation System</h1>
   </div>
-  <div class="iot-data">
-    <h2>Current Soil Moisture</h2>
-<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2667716/charts/1?bgcolor=%23ffffff&color=%23d62020&days=5&dynamic=true&results=100&type=line&xaxis=Time&yaxis=Moisture"></iframe>
-    <div class="water-switch-container">
-      <div>
-        <p>Watering Status: </p>
-        <b>
-          <p id="watering-status"></p>
-        </b>
-        <div class="loader" id="water-switch-loader"></div>
+  <div style="display: flex; flex-direction: row; justify-content: space-around;">
+    <div class="iot-data">
+      <h2>Current Soil Moisture</h2>
+      <iframe width="450" height="260" "
+      src="
+        https://thingspeak.com/channels/2667716/charts/1?bgcolor=%23ffffff&color=%23d62020&days=14&dynamic=true&results=100&type=line&xaxis=Time&yaxis=Moisture"></iframe>
+      <div class="water-switch-container">
+        <div>
+          <p>Watering Status: </p>
+          <b>
+            <p id="watering-status"></p>
+          </b>
+          <div class="loader" id="water-switch-loader"></div>
+        </div>
+        <button id="water-switch"></button>
       </div>
-      <button id="water-switch"></button>
     </div>
+    <div>
+      <h1>State Machine Configuration</h1>
+      <iframe id="errorOutput" name="errorOutput" frameBorder="0"> </iframe>
+      <div>
+        <p>Current Maximum: </p>
+        <b>
+          <p id="current-max"></p>
+        </b>
+        <p>Current Minimum: </p>
+        <b>
+          <p id="current-min"></p>
+        </b>
+      </div>
+      <h2>Edit State Machine Values</h2>
+      <form method="post" action="updatebounds.php" accept-charset="utf-8" target="errorOutput" onsubmit="fetchCurrentSMBounds();">
+        State Machine Maximum: <input type="text" name="upperBound"><br>
+        State Machine Minimum: <input type="text" name="lowerBound"><br>
+        <input type="submit" value="Apply">
+      </form>  	
+    </div>
+  </div>
 </body>
 
 <script>
   document.addEventListener("DOMContentLoaded", function () {
-    valsOnPageLoad()
+    valsOnPageLoad();
     fetchWateringStatus().then(function (response) {
       setValsOnRequest(response);
     });
+    fetchCurrentSMBounds();
   });
 
   document.getElementById("water-switch").onclick = function () { fetchSwitchWatering() };
 
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  function fetchCurrentSMBounds() {
+  	var currMax = document.getElementById("current-max");
+  	var currMin = document.getElementById("current-min");
+  	fetch("fetchbounds.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: "",
+    })
+      .then((response) => {
+        try {
+          JSON.parse(response);
+        } catch (error) {
+        console.log(error);
+        currMax.innerText = "ERROR: Unable to retreive current SM Configuration.";
+        currMin.innerText = "ERROR: Unable to retreive current SM Configuration.";
+        }
+        return response.json();
+      })
+      .then((response) => {
+        currMax.innerText = response.upperBound;
+        currMin.innerText = response.lowerBound;
+      });
   }
 
   async function fetchWateringStatus() {
@@ -134,18 +191,20 @@
   }
 
   async function waitForWaterStatusChange(new_state) {
-    timeout_max_retries = 10;
-    timeout_time_per_retry = 2500;
+    // We can only write every 15 seconds, so the delay for check is 17 seconds initally
+    // and then 5 seconds on retry
+    timeout_max_retries = 3;
+    timeout_time_per_retry = 5000;
 
-    await sleep(5000)
+    await sleep(10000)
     var status;
     for (let i = 0; i < timeout_max_retries; i++) {
       status = await fetchWateringStatus();
       if (status == new_state) {
-        setValsOnRequest(status);
+        setWateringButtonText(status);
         return true;
       }
-      await sleep(timeout_time_per_retry)
+      await sleep(5000)
     }
     setValsOnRequest(status);
     alert("Error: Watering status could not be updated.")
