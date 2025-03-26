6840b5d96dd9c1b8aa7be3982d4ba4ef9054b7be
aster-isk <spinkmaddie00@gmail.com>
2025-03-26 14:47:19 -0700

    Fixed error messgage bug in set bounds php.

--- webserver/html/updatebounds.php
+++ webserver/html/updatebounds.php
@@ -3,17 +3,17 @@ $json_string = json_encode($_POST);
 $sucess = 1;
 
 if($_POST["upperBound"] <= $_POST["lowerBound"]) {
-	echo sprintf('<div style="background-color: #EE4B2B;">Invalid range [%d, %d]. State Machine maximum must be lower than state machine minumum.</div>', $_POST["lowerBound"], $_POST["upperBound"]);
+	echo sprintf('<div style="background-color: #EE4B2B;">Invalid range [%s, %s]. State Machine maximum must be lower than state machine minumum.</div>', $_POST["lowerBound"], $_POST["upperBound"]);
 
 } else if ($_POST["upperBound"] > 1 || $_POST["lowerBound"] < 0) {
-	echo sprintf('<div style="background-color: #EE4B2B;">Invalid range [%d, %d] for state machine. Bounds must fit in the range [0, 1].</div>', $_POST["lowerBound"], $_POST["upperBound"]);
+	echo sprintf('<div style="background-color: #EE4B2B;">Invalid range [%s, %s] for state machine. Bounds must fit in the range [0, 1].</div>', $_POST["lowerBound"], $_POST["upperBound"]);
 } else {
   try {
     $file_handle = fopen('/var/lib/irrigator/sm_config.json', 'w');
     fwrite($file_handle, $json_string);
     fclose($file_handle);
     $sucess = 0;
-	echo sprintf('Sucessfully updated state machine bounds to [%d, %d]', $_POST["lowerBound"], $_POST["upperBound"]);
+	echo sprintf('Sucessfully updated state machine bounds to [%s, %s]', $_POST["lowerBound"], $_POST["upperBound"]);
   }
   catch(Exception $e) {
     echo $e;
