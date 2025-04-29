ab16ccf23cac917fe9f044bb22b814725f3e3e26
aster-isk <spinkmaddie00@gmail.com>
2025-04-29 15:00:07 -0700

    Fixed bug in start bash file.

--- startMatlabApp.sh
+++ startMatlabApp.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-MATLAB_PATH = "example/path/MATLAB/R2024b/bin/matlab"
-IRRIGATORS_PATH = "example/path/irrigators"
+MATLAB_PATH="example/path/MATLAB/R2024b/bin/matlab"
+IRRIGATORS_PATH="example/path/irrigators"
 
-if [ ! -f "./matlab.log" ]; then
-  touch "./matlab.log"
+if [ ! -f "${IRRIGATORS_PATH}/matlab.log" ]; then
+  touch "${IRRIGATORS_PATH}/matlab.log"
 fi
 
-$MATLAB_PATH -nodisplay -nosplash -nodesktop -r "run('${IRRIGATORS_PATH}/src/WebServerComm.m'); exit();"
+$MATLAB_PATH -nodisplay -nosplash -nodesktop -r "run('${IRRIGATORS_PATH}/src/Irrigator.m'); exit();" > "${IRRIGATORS_PATH}/matlab.log" 2>&1
